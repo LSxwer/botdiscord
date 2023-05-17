59fb16e2ce41b94204ed6a8f2725b1816caabff2
LSxwer <126370973+LSxwer@users.noreply.github.com>
2023-05-17 21:50:14 +0700

    Update cmd.js

--- cmd.js
+++ cmd.js
@@ -30,12 +30,12 @@ const commands = [
 
 const rest = new REST().setToken(token);
 
-// and deploy your commands!
+
 (async () => {
 	try {
 		console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
-		// The put method is used to fully refresh all commands in the guild with the current set
+		
 		const data = await rest.put(
 			Routes.applicationGuildCommands(clientId, guildId),
 			{ body: commands },
@@ -43,7 +43,7 @@ const rest = new REST().setToken(token);
 
 		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
 	} catch (error) {
-		// And of course, make sure you catch and log any errors!
+		
 		console.error(error);
 	}
 })();

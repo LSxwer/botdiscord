e374fc223c352c3b5b766ef5a62a5c32626fafde
LSxwer <126370973+LSxwer@users.noreply.github.com>
2023-05-17 21:46:59 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,90 +1,40 @@
-require('dotenv').config();
-const { Client, IntentsBitField, SlashCommandBuilder, REST , Routes   } = require('discord.js');
 
-const client = new Client({
-  intents: [
-    IntentsBitField.Flags.Guilds,
-    IntentsBitField.Flags.GuildMembers,
-    IntentsBitField.Flags.GuildMessages,
-    IntentsBitField.Flags.MessageContent,
-    IntentsBitField.Flags.GuildPresences,
-  ],
+const { Client, Events, GatewayIntentBits, userMention, MentionableSelectMenuBuilder } = require('discord.js');
+const { token } = require('./config.json');
+
+const rand = Math.random().toString().substr(1, 4)
+
+
+
+
+
+
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+
+client.once(Events.ClientReady, c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
-client.on('ready', (c) => {
-  console.log(`✅ ${c.user.tag} is online.`);
-});
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
 
-client.on('interactionCreate', (interaction) => {
-  if (!interaction.isChatInputCommand()) return;
+	if (interaction.commandName === 'perm') {
+		await interaction.reply({ content: '@everyone', ephemeral: false });
+	}
+	if (interaction.commandName === 'elochecker'){
+		await interaction.reply({ content : 'elo:' ,ephemeral:false}); 
+	}
+	
+	
+	
+	
+	
 
-  if (interaction.commandName === 'hey') {
-    return interaction.reply('hey!');
-  }
-
-  if (interaction.commandName === 'ping') {
-    return interaction.reply('Pong!');
-  }
-  if (interaction.commandName === 'minh'){
-    return interaction.reply({ content: 'Secret Pong!', ephemeral: true });
-    return interaction.deleteReply(5)
-    
-  } 
-  
-});
-
-
-client.on('messageCreate', (message) => {
-  if (message.author.bot) {
-    return;
-  }
-
-  if (message.content === 'hello') {
-    message.reply('hello');
-  }
+	 
 });
 
 
 
-const commands = [
-  {
-    name: 'hey',
-    description: 'Replies with hey!',
-  },
-  {
-    name: 'ping',
-    description: 'Pong!',
-  },
-  {
-    name: 'minh',
-    description: 'ping minh',
-    
-  }
-];
-
-const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
-
-(async () => { 
-  try {
-    console.log('Registering slash commands...');
-
-    await rest.put(
-      Routes.applicationGuildCommands(
-        process.env.CLIENT_ID,
-        process.env.GUILD_ID
-      ),
-      { body: commands }
-    );
-
-    console.log('Slash commands were registered successfully!');
-  } catch (error) {
-    console.log(`There was an error: ${error}`);
-  }
-})(); 
-
-
-
-
-
-
-client.login(process.env.TOKEN);
+client.login(token);
